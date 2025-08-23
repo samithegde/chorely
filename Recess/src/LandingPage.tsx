f4c2cd958551cba4a0d52b885f2d994d33ffa711
samit <supersamitrocks@gmail.com>
2025-08-23 14:22:08 -0400

    Finished first version of landing page

--- Recess/src/LandingPage.tsx
+++ Recess/src/LandingPage.tsx
@@ -5,14 +5,17 @@ export default function LandingPage() {
     <div
       style={{
         height: "calc(100vh - 64px)",
+        width: "100vw",
         display: "flex",
-        alignItems: "center",
         justifyContent: "space-between",
+        alignItems: "center",
         padding: "0 80px",
         background: "linear-gradient(to bottom, #1c1c1c, #2a2a2a)",
         color: "#fff",
+        boxSizing: "border-box",
       }}
     >
+      {/* Left: Text */}
       <div style={{ maxWidth: "400px" }}>
         <h1 style={{ fontSize: "2.5rem", marginBottom: "16px" }}>
           Get Your <br /> First Gig.
@@ -39,8 +42,8 @@ export default function LandingPage() {
       <div
         style={{
           flexShrink: 0,
-          width: "500px",
-          height: "350px",
+          width: "50%",
+          height: "70%",
           borderRadius: "16px",
           overflow: "hidden",
           boxShadow: "0 8px 20px rgba(0,0,0,0.3)",
